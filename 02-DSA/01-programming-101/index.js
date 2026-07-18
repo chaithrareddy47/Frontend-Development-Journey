aa2ff9f903ba7f0af5bad9ec3dbbd8a553a1921f
chaithrareddy47 <chaithrareddy475@gmail.com>
2026-07-18 23:01:42 +0530

    DSA

---
+++ 02-DSA/01-programming-101/index.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+console.log(true);// boolean
+
+console.log(7);// number
+
+
+// 3 different data types string , number boolean
+
+const a = 10; // `10 is assigned to a variable
+console.log(a);
