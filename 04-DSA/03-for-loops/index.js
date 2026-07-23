9aebac26aaced5582904c991d1a34e4516af763d
chaithrareddy47 <chaithrareddy475@gmail.com>
2026-07-23 23:58:07 +0530

    DSA for...loops

---
+++ 04-DSA/03-for-loops/index.js
@@ -0,0 +1,35 @@
+// for loops 
+let x = 5;
+for (let i = 0; i < x; i++){
+    console.log(i + " " + "Hello World" + " "+ x);
+    
+}
+
+for(let i = 3; i < 5; i++) {
+    console.log("Hiee");
+    
+}
+for (let i = 2; i < 9; i=i+2){
+    console.log("hw" + " "+ (i+2));
+    
+}
+
+for (let i = 5; i > 0; i--){
+    console.log("Namaste");
+    
+}
+
+for (let i = 5; i < 4; i++){
+    console.log("hi");
+    
+}
+for (let i = 0; i < 0; i++){
+    console.log("hi");
+    
+}
+for (let i = 1; i > 0; i++){
+    console.log("hi");
+    
+}
+
+
